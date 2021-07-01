2334fc537bf64eb53247dee44b3c21d67d99db78
l0rdluc1f3r <lordlucifer0704@gmail.com>
2021-07-02 00:17:19 +0530

    react basic project

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        id: 1,
+        name: 'Nick Fury',
+        age: 70,
+        image:
+        'https://imgix.ranker.com/user_node_img/84/1676112/original/nick-fury-photo-u41?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+   },
+{
+    id: 2,
+    name: 'Groot',
+    age: 4,
+    image:
+    'https://imgix.ranker.com/user_node_img/56/1106088/original/groot-photo-u40?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 3,
+    name: 'Black Widow',
+    age: 34,
+    image:
+    'https://imgix.ranker.com/user_node_img/137/2725051/original/black-widow-photo-u88?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 4,
+    name: 'Loki',
+    age: 1053,
+    image:
+    'https://imgix.ranker.com/user_node_img/149/2968986/original/loki-photo-u58?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 5,
+    name: 'Vision',
+    age: 3,
+    image:
+    'https://imgix.ranker.com/user_node_img/118/2344305/original/vision-photo-u40?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 6,
+    name: 'Tony Stark',
+    age: 48,
+    image:
+    'https://imgix.ranker.com/user_node_img/62/1237308/original/iron-man-photo-u80?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 7,
+    name: 'Hela',
+    age: 5000,
+    image:
+    'https://imgix.ranker.com/user_node_img/3694/73861457/original/hela-photo-u2?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 8,
+    name: 'Thor',
+    age: 1000,
+    image:
+    'https://imgix.ranker.com/user_node_img/112/2239723/original/thor-photo-u86?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 9,
+    name: 'Steve Rogers',
+    age: 100,
+    image:
+    'https://imgix.ranker.com/user_node_img/34/669163/original/captain-america-photo-u94?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+{
+    id: 10,
+    name: 'Odin',
+    age: 5000,
+    image:
+    'https://imgix.ranker.com/user_node_img/140/2796159/original/odin-photo-u17?auto=format&fit=crop&fm=pjpg&w=375&q=60&dpr=1',
+
+},
+
+];
